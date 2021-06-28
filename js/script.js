4be7b64f921947cdbfae2d6d2f151d747fd2e0b6
Janet Mwangi <janomwangi@gmail.com>
2021-06-28 18:13:50 +0300

    adjusted pizza option prices

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $("form#buy-items").submit(function(event){
+        event.preventDefault()
+var pizzaSize=$(".pizza-size").val();
+var crustType=$(".crust-type").val();
+var toppings=$(".toppings").val();
+
+var newPizza=new Pizza(pizzaSize, crustType,toppings)
+
+console.log(newPizza.totalprice())
+
+    });
+
+    // $('#itemList').append("<p> Pizza </p>");
+});
+
+//constructor
+function Pizza(pizzaSize, crustType,toppings){
+
+    this.pizzaSize=pizzaSize;
+    this.crustType=crustType;
+    this.toppings=toppings;
+}
+
+//prototype
+
+Pizza.prototype.totalprice=function(){
+    var total=parseInt(this.pizzaSize) + parseInt(this.crustType)+ parseInt(this.toppings)
+    return total
+}
