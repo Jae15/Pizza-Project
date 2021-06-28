d0d88bd9f65422f753f2717beee57871e6c698d2
Janet Mwangi <janomwangi@gmail.com>
2021-06-28 20:20:57 +0300

    corrected js errors

--- js/script.js
+++ js/script.js
@@ -10,17 +10,18 @@ var newPizza=new Pizza(pizzaSize, crustType,toppings)
 console.log(newPizza.totalprice())
 
 });
+});
 
 //constructor
 function Pizza(pizzaSize, crustType,toppings){
     this.pizzaSize=pizzaSize;
     this.crustType=crustType;
     this.toppings=toppings;
-}
+};
 
 //prototype
 
 Pizza.prototype.totalprice=function(){
     var total=parseInt(this.pizzaSize) + parseInt(this.crustType)+ parseInt(this.toppings)
     return total
-}
+};
