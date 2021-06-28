9bd18149c2ec3efc3d32c01b18edc152dfce248c
Janet Mwangi <janomwangi@gmail.com>
2021-06-28 18:45:05 +0300

    added item list on js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ var crustType=$(".crust-type").val();
 var toppings=$(".toppings").val();
 
 var newPizza=new Pizza(pizzaSize, crustType,toppings)
+
+console.log(newPizza.totalprice())
+
+});
+
+$('#itemList').append("<p> Pizza </p>");
