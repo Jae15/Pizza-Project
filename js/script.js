97becc61d2a52036c712ca8dda3abce2ba249ef7
Janet Mwangi <janomwangi@gmail.com>
2021-07-01 11:33:10 +0300

    added the pizza number multiplier on js and added total price display function

--- js/script.js
+++ js/script.js
@@ -4,24 +4,41 @@ $(document).ready(function () {
 var pizzaSize=$(".pizza-size").val();
 var crustType=$(".crust-type").val();
 var toppings=$(".toppings").val();
+var pizzanumber=$(".pizzano").val();
 
-var newPizza=new Pizza(pizzaSize, crustType,toppings)
+
+
+var newPizza=new Pizza(pizzaSize, crustType,toppings,pizzanumber)
 
 console.log(newPizza.totalprice())
+
+console.log("NumberofPizzas",pizzanumber)
 
 });
 });
 
 //constructor
-function Pizza(pizzaSize, crustType,toppings){
+function Pizza(pizzaSize, crustType,toppings,pizzanumber){
     this.pizzaSize=pizzaSize;
     this.crustType=crustType;
     this.toppings=toppings;
+    this.pizzanumber=pizzanumber;
+
 };
 
 //prototype
 
 Pizza.prototype.totalprice=function(){
-    var total=parseInt(this.pizzaSize) + parseInt(this.crustType)+ parseInt(this.toppings)
+    var total=parseInt(this.pizzaSize) + parseInt(this.crustType)+ parseInt(this.toppings);
+    document.getElementById("totalprice").innerHTML=total*parseInt(this.pizzanumber);
     return total
+
+
 };
+
+
+
+// alert("totalprice()")
+
+
+// $document.getElementById("output").innerHTML="Your Total is" +total;
